7fe13f12cec7a4fc28ea441f8fa4a68edac82243
dviverosm <dviverosm@unicauca.edu.co>
2016-08-29 10:16:09 -0500

    Add files via upload

---
+++ src/co/edu/unicauca/prematricula/util/C.java
@@ -0,0 +1,15 @@
+package util;
+
+public class C 
+{
+	public static final int PROGRAMA_SISTEMAS = 38;
+	public static final int PROGRAMA_ELECTRONICA = 37;
+	public static final int PROGRAMA_AUTOMATICA = 34;
+	public static final int PROGRAMA_TELEMATICA = 26;
+	
+	public static final String PROGRAMA_SISTEMAS_STR = "Ingeniería de Sistemas";
+	public static final String PROGRAMA_ELECTRONICA_STR = "Ingeniería Electrónica y Telecomunicaciones";
+	public static final String PROGRAMA_AUTOMATICA_STR = "Ingeniería en Automática Industrial";
+	public static final String PROGRAMA_TELEMATICA_STR = "Tecnología en Telemática";
+	
+}
